a2b67b633ac927cde8ad28a64f50f074593cb555
Vlad <ivm@irbis.plus>
2026-09-21 22:48:05 +0400

    feat: add menu page

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/rsschool-landing-page",
   build: {
     sourcemap: true,
     outDir: "docs",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        menu: resolve(__dirname, "menu.html"),
+      },
+    },
   },
 });
